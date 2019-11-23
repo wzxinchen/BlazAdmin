e04de7ea767faf4c174904818205103899648f60
zhang.zhe <zhang.zhe@synyi.com>
2019-11-23 21:08:07 +0800

    初始化

---
+++ src/BlazAdmin/BAdminTemplateBase.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazAdmin
+{
+    public class BAdminTemplateBase : ComponentBase
+    {
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+        [Parameter]
+        public string DefaultRoute { get; set; }
+
+        [Parameter]
+        public RenderFragment Body { get; set; }
+
+        [Inject]
+        NavigationManager NavigationManager { get; set; }
+        protected override void OnInitialized()
+        {
+            if (new Uri(NavigationManager.Uri).LocalPath == "/" && !string.IsNullOrWhiteSpace(DefaultRoute))
+            {
+                NavigationManager.NavigateTo(DefaultRoute);
+            }
+        }
+    }
+}
---
+++ src/BlazAdmin/ExtensionBuilder.cs
@@ -0,0 +1,20 @@
+﻿using Blazui.Component;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazAdmin
+{
+    public static class ExtensionBuilder
+    {
+        public static IServiceCollection AddBlazAdminServices(this IServiceCollection services)
+        {
+            services.AddRazorPages();
+            services.AddServerSideBlazor();
+            services.AddHttpClient();
+            services.AddBlazuiServices();
+            return services;
+        }
+    }
+}
